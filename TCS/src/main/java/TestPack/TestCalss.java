1238e11ce7abf3d9ea8dd8479080676bde6f79f6
HP <HP@DESKTOP-EG3JDB2>
2023-11-14 14:48:57 +0530

    New project first commit

---
+++ TCS/src/main/java/TestPack/TestCalss.java
@@ -0,0 +1,27 @@
+package TestPack;
+
+public class TestCalss {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Class is for test");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+}
